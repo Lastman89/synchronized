8b52b3f3c5d69f9fa996c9a24f14657263394f20
Lastman89 <alexeygr89@gmail.com>
2024-02-15 19:25:50 +0300

    Доработка и осознание

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,49 +13,49 @@ public class Main {
         // Создаём пул потоков
         List<Thread> threads = new ArrayList<>();
 
-
         for (int i = 0; i < countTrack; i++) {
             //задание для каждого нового потока
             Thread thread = new Thread(() -> {
                 String maxLeght = generateRoute("RLRFR", 100);
                 int Count = (int) maxLeght.chars().filter(ch -> ch == find).count();
                 //критическая секция мапы, запускаем по одному потоку
-                synchronized (threads) {
+                synchronized (sizeToFreq) {
                     if (sizeToFreq.containsKey(Count)) {
                         sizeToFreq.put(Count, (sizeToFreq.get(Count) + 1));
                     } else {
                         sizeToFreq.put(Count, 1);
                     }
-                    threads.notify(); //сообщаем о возможности работы вторым потоком
+                    sizeToFreq.notify(); //сообщаем о возможности работы вторым потоком
                 }
-
             });
-            Thread thread_2 = new Thread(() -> {
-                while (!Thread.interrupted()) {
-                    synchronized (threads) {
-                        if (threads.isEmpty()) {
-                            try {
-                                threads.wait();//ожидание сигнала о возможности обработки
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        System.out.println("Текущий лидер: " +
-                                sizeToFreq.entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).get().getKey());
-                    }
-                }
-            }
-            );
-            thread_2.start();
-            threads.add(thread);//добавляем в массив потоков каждый новый
             thread.start();//стартуем поток
+            threads.add(thread);//добавляем в массив потоков каждый новый
 
-            for (Thread thread_1 : threads) {
-                thread_1.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
-            }
-            thread_2.interrupt();//прерываем поток
         }
 
+        Thread threadSecond = new Thread(() -> {
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    if (sizeToFreq.isEmpty()) {
+                        try {
+                            sizeToFreq.wait();//ожидание сигнала о возможности обработки
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Текущий лидер: " +
+                            sizeToFreq.entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).get().getKey());
+                }
+            }
+        }
+        );
+        threadSecond.start();
+
+        for (Thread threadFirst : threads) {
+            threadFirst.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
+        }
+
+        threadSecond.interrupt();//прерываем поток
 
         System.out.println("Самое частое количество повторений " +
                 sizeToFreq.entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).get().getKey() +
